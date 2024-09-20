1dce6b8929d883baa7508503359f74628e128b93
Filip Fagerström <filip.fagerstrom@elev.ga.ntig.se>
2024-09-20 10:35:24 +0200

    dyjdtj7

---
+++ Fighting/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int card = Random.Shared.Next(52);
+
+
+Console.WriteLine("Skriv in ditt namn här:");
+
+string name = Console.ReadLine();
+while (name.Length < 3)
+{
+    Console.WriteLine("Skriv in ditt namn för fan din pungråtta");
+    name = Console.ReadLine();
+}
+
+
+Console.WriteLine($"Hej {name}");
+
+Console.ReadLine();
