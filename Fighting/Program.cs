9eea66ff860ffbec9afdbf05df92e9e6c5426edb
Filip Fagerström <filip.fagerstrom@elev.ga.ntig.se>
2024-09-23 09:08:36 +0200

    help i have lost my fingers

--- Fighting/Program.cs
+++ Fighting/Program.cs
@@ -2,9 +2,10 @@
 int card = Random.Shared.Next(52);
 int you = Random.Shared.Next(10);
 int bosse = Random.Shared.Next(10);
-int bosseHP = Random.Shared.Next(100);
-int youHP = Random.Shared.Next(100);
+int bosseHP = 100;
+int youHP = 100;
 int idk = 0;
+int round = 1;
 
 
 Console.WriteLine("Skriv in ditt namn här:");
@@ -44,15 +45,41 @@ Console.WriteLine($"Det tog {idk+1} gånger, {idk} för många");
 Console.WriteLine("");
 Console.WriteLine("Du ska möta Bosse the CP! Han är stark (ouch)");
 
+bosseHP = bosseHP;
+youHP = youHP;
+
 while (youHP > 0 || bosseHP > 0)
 {
-    Console.WriteLine("Round 1:");
-    Console.WriteLine("Round 1:");
+    Console.WriteLine($"Round {round}:");
+    Console.WriteLine("How will you attack your opponent?");
     Console.WriteLine("A. Kick     B. Bonk     C. Bite"); string youM = Console.ReadLine();
-    if (youM.ToLower() == "a" || youM.ToLower() == "b" || youM.ToLower() == "c") {bosseHP = bosseHP - you;}
+    if (youM.ToLower() == "a" || youM.ToLower() == "b" || youM.ToLower() == "c")
+    {
+        bosseHP = bosseHP - Random.Shared.Next(10);
+        youHP = youHP - Random.Shared.Next(10);
+        round ++;
+        Console.WriteLine($"Your opponent has {bosseHP} HP left!");
+        Console.WriteLine($"You have {youHP} HP left!");
+    }
+    else
+    {
+        Console.WriteLine("Attack them! Let's redo it, go for the finishing move!");
+    }
+
+    if (bosseHP > 0 && youHP <= 0)
+    {
+        Console.WriteLine($"{name} has gotten ultrakilled!");
+    }
+    else if (youHP > 0 && bosseHP <= 0)
+    {
+        Console.WriteLine("Bosse the CP has gotten ultrakilled!");
+    }
+    else if (youHP <= 0 && bosseHP <= 0)
+    {
+        Console.WriteLine("Both have gotten ultrakilled!");
+    }
+
     Console.WriteLine("");
 }
-
-
 
 Console.ReadLine();
